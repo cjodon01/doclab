39302cd76e729ad55694592d7f78f245c5062138
Cory A. Jodon <137443282+cjodon01@users.noreply.github.com>
2025-07-02 12:23:48 -0400

    Fix module resolution and build configuration

--- dist/lib/markdown.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function parseMarkdown(content: string): Promise<string>;
-export declare function extractTitle(content: string): string;
-export declare function formatFileName(fileName: string): string;
-//# sourceMappingURL=markdown.d.ts.map
